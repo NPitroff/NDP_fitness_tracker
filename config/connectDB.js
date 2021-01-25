f948ef65f605b689d65392bb7449eefbc3e1e6d8
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-24 18:53:39 -0800

    Added a congig folder with a connectDB

---
+++ config/connectDB.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require('dotenv/config');
+
+const dbConfig = process.env.MONGODV_URI;
+
+async function connectDB() {
+    await mongoose.connect(dbConfig, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true
+    }, () =>
+    console.log("DB CONNECTED")
+    );
+}
+
+module.exports = connectDB;
