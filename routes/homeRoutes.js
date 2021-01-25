83addff820f20638cd9484ba64c51591cd2c4bc6
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-24 21:09:51 -0800

    Added a route for the 'get' workouts

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -12,8 +12,20 @@ router.get('/', (req, res)=>{
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-  
+
   
   router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+  router.get("/api/workouts", (req, res) => {
+    console.log(req.body);
+  
+    Exercise.find({})
+      .then(workout => {
+        res.json(workout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
