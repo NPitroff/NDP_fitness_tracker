ea779652a492c773ceb216b5eda89bba97c093ab
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-24 21:42:33 -0800

    Added a dbConnect

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -64,4 +64,13 @@ router.get("/exercise", (req, res) => {
       });
   });
   
-  module.exports = router;
+//   module.exports = router;
+
+/******************************* Connect to db  ****************************/
+connectDB()
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
