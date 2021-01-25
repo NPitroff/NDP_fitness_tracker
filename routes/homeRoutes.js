8fc250e731fa66298ee21c88eb01a7cf107e374a
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-24 21:06:42 -0800

    added a path to the index.html

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
 const db = require("../models");
+const Exercise = require("../models/exerciseData");
 
-//
+//routes to get and push data to the db============
+
+router.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
