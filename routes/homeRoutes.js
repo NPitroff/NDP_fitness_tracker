30c7d972faa9ec935113917df8269d2f3a0387bd
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-24 22:08:40 -0800

    finalizing switch in routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,68 +1,76 @@
-const router = require('express').Router();
-const db = require("../models");
-const Exercise = require("../models/exerciseData");
+// const router = require('express').Router();
+// const db = require("../models");
+// const Exercise = require("../models/exerciseData");
 
 //routes to load the different pages============
 
-router.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+// router.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
 
-});
+// });
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/exercise.html'));
-  });
+// router.get("/exercise", (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/exercise.html'));
+//   });
 
   
-  router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/stats.html'));
-  });
+//   router.get("/stats", (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/stats.html'));
+//   });
+
+
 //route to get the workout data==================
-  router.get("/api/workouts", (req, res) => {
-    console.log(req.body);
+//   router.get("/api/workouts", (req, res) => {
+//     console.log(req.body);
   
-    db.Exercise.find({})
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+//     db.Exercise.find({})
+//       .then(workout => {
+//         res.json(workout);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+
+
   //route to create new exercise data===========
-  router.post("/api/workouts", (req, res) => {
-    console.log(req.body);
+//   router.post("/api/workouts", (req, res) => {
+//     console.log(req.body);
   
-    db.Exercise.create({
-      exercises: []
-    })
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+//     db.Exercise.create({
+//       exercises: []
+//     })
+//       .then(workout => {
+//         res.json(workout);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+
+
   //route to push new data into the array of exercises================
-  router.put("/api/workouts/:id", (req, res) => {
-    db.Exercise.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } })
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+//   router.put("/api/workouts/:id", (req, res) => {
+//     db.Exercise.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } })
+//       .then(workout => {
+//         res.json(workout);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+
+
   //route to find all exercises========================
-  router.get("/api/workouts/range", (req, res) => {
-    db.Exercise.find({})
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+//   router.get("/api/workouts/range", (req, res) => {
+//     db.Exercise.find({})
+//       .then(workout => {
+//         res.json(workout);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
   
 //   module.exports = router;
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-const path = require('path');
-const Exercise = require('./models/exerciseData');
+const path = require("path");
+const Exercise = require("./models/exerciseData");
 const mongoose = require("mongoose");
 // const routes = require("./routes/index");
 // const connectDB = require("./config/connectDB")
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 //link models
-const db = require("./models")
+const db = require("./models");
 
 app.use(logger("dev"));
 //sets up use of the routes folder=============
@@ -24,17 +24,85 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/ndpexercise-tracker-01',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+  process.env.MONGODB_URI || "mongodb://localhost/ndpexercise-tracker-01",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 //routes==============================================
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+//create new exercise==============
+app.get("/api/workouts", async (req, res) => {
+  Exercise.find({})
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", async (req, res) => {
+  const exercise = new Exercise({ exercises: req.body });
+  Exercise.create(exercise)
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  Exercise.findById(id)
+    .then((dbExercise) => {
+      dbExercise.exercises.push(data);
+      return dbExercise;
+    })
+    .then((dbExercise) => {
+      Exercise.findOneAndUpdate(
+        { _id: id },
+        { exercises: dbExercise.exercises },
+        { new: true }
+      )
+        .then((dbExercise) => {
+          res.json(dbExercise);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts/range", async (req, res) => {
+  Exercise.find({})
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
