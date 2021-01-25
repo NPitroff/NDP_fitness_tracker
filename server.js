e128a9daa85f589da2fd5d1fc1dfda40af7f1fef
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-25 07:41:46 -0800

    changed router to app in the paths

--- server.js
+++ server.js
@@ -35,16 +35,16 @@ mongoose.connect(
   }
 );
 //routes==============================================
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
-router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/stats.html"));
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
 //create new exercise==============
